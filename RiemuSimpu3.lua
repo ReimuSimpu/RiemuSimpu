95403d78497adea495afbd3bdeedb88bff107bbd
靈夢辛普 <146130238+ReimuSimpu@users.noreply.github.com>
2024-01-11 11:14:35 -0600

    Update RiemuSimpu3.lua

--- RiemuSimpu3.lua
+++ RiemuSimpu3.lua
@@ -6,8 +6,8 @@ getgenv().normalwebhook = ""
 getgenv().snipeNormalPets = false
 repeat wait() until game:IsLoaded()
 if game.PlaceId == 15502339080 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/ReimuSimpu/RiemuSimpu/main/靈夢辛普.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ReimuSimpu/RiemuSimpu/main/RiemuSimpu.lua"))()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Root1527/personal/main/lowcpu.lua"))()
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/ReimuSimpu/RiemuSimpu/main/靈夢辛普2.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ReimuSimpu/RiemuSimpu/main/RiemuSimpu2.lua"))()
 end
