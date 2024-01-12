0d235c20d2de8ad4d41f40e4624bbb48227e7a54
靈夢辛普 <146130238+ReimuSimpu@users.noreply.github.com>
2024-01-12 15:29:48 -0600

    Update slimmy.lua

--- slimmy.lua
+++ slimmy.lua
@@ -183,7 +183,7 @@ Booths_Broadcast.OnClientEvent:Connect(function(username, message)
                 elseif type.huge and unitGems <= 3000000 then
                     coroutine.wrap(tryPurchase)(uid, gems, item, version, shiny, amount, username, class, playerid, buytimestamp, listTimestamp)
                     return
-		            end
+		end
 
                 -- Presents and Eggs
             elseif (item == "Titanic Christmas Present" or string.find(item, "2024 New Year")) and unitGems <= 30000 then
