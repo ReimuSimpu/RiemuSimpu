5ec82e6ee7c6e890df01f166421694794ae307dd
靈夢辛普 <146130238+ReimuSimpu@users.noreply.github.com>
2024-01-13 00:51:21 -0600

    Update RiemuSimpu3.lua

--- RiemuSimpu3.lua
+++ RiemuSimpu3.lua
@@ -2,8 +2,6 @@ getgenv().webhook = "https://discord.com/api/webhooks/1194781509316526203/FgcR0N
 getgenv().webhookFail = "https://discord.com/api/webhooks/1194782345060954203/80SRvIBmfAIQqzb_5NzjOuGJ8sfCoetumwPRdY16Kz4wYuFx-GJ1tFczXh6NDQwSOvdS"
 getgenv().userid = "618580498251382824"
 getgenv().alts = {"Badlandschunks", "Birb_Snek", "FJB_3", "SatouMatsuzaka5", "SatouMatsuzaka6", "SatouMatsuzaka7", "SatouMatsuzaka69", "ShioKobe69", "Drbigg01", "Drbigg02", "Drbigg03", "Drbigg04", "Drbigg05", "Drbigg06", "Drbigg07", "Drbigg08", "Drbigg09", "Drbigg10", "Drbigg11", "Drbigg12", "Drbigg13", "Drbigg14", "Drbigg15", "Drbigg16", "Drbigg17", "Drbigg18", "Drbigg19", "Drbigg20", "Drbigg21", "Drbigg22", "Drbigg23", "Drbigg024"}
-getgenv().normalwebhook = ""
-getgenv().snipeNormalPets = false
 repeat wait() until game:IsLoaded()
 if game.PlaceId == 15502339080 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ReimuSimpu/RiemuSimpu/main/RiemuSimpu.lua"))()
