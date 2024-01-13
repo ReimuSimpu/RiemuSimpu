70618b48fcdccd8f1d9ae6c25581f7b3c6902b1c
靈夢辛普 <146130238+ReimuSimpu@users.noreply.github.com>
2024-01-12 21:11:29 -0600

    Create slimmy2.lua

---
+++ slimmy2.lua
@@ -0,0 +1,11 @@
+getgenv().webhook = "https://discord.com/api/webhooks/1195565325811462214/TyHWT1-oLK20xJzdBqC58F6t9nFK6Im22IAAbY0ayZFYdm3-XOdrzBypNQ4_slenrtzA"
+getgenv().webhookFail = "https://discord.com/api/webhooks/1194782345060954203/80SRvIBmfAIQqzb_5NzjOuGJ8sfCoetumwPRdY16Kz4wYuFx-GJ1tFczXh6NDQwSOvdS"
+getgenv().userid = "618580498251382824"
+getgenv().alts = {"Badlandschunks", "Birb_Snek", "FJB_3", "SatouMatsuzaka5", "SatouMatsuzaka6", "SatouMatsuzaka7", "SatouMatsuzaka69", "ShioKobe69", "Drbigg01", "Drbigg02", "Drbigg03", "Drbigg04", "Drbigg05", "Drbigg06", "Drbigg07", "Drbigg08", "Drbigg09", "Drbigg10", "Drbigg11", "Drbigg12", "Drbigg13", "Drbigg14", "Drbigg15", "Drbigg16", "Drbigg17", "Drbigg18", "Drbigg19", "Drbigg20", "Drbigg21", "Drbigg22", "Drbigg23", "Drbigg024"}
+repeat wait() until game:IsLoaded()
+if game.PlaceId == 15502339080 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ReimuSimpu/RiemuSimpu/main/slimmy.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ReimuSimpu/RiemuSimpu/main/resourcelower.lua"))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ReimuSimpu/RiemuSimpu/main/RiemuSimpu2.lua"))()
+end
